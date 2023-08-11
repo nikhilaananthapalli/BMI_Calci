d357c6613bacaafb19c61ff1599058fd1f59c607
nikhilaananthapalli <140099221+nikhilaananthapalli@users.noreply.github.com>
2023-08-11 14:33:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './Components/Bmi_Calci'
+import Bmi_Calci from './Components/Bmi_Calci';
+
+function App() {
+  return (
+    <div className="App">
+    <Bmi_Calci/>
+    </div>
+  );
+}
+
+export default App;
